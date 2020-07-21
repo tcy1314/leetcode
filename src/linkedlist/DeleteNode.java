1fbda947d44ff22ca2eb28f844bcd11923e3d9a0
tcy <tcy0117@163.com>
2020-07-21 22:18:04 +0800

    init project

---
+++ src/linkedlist/DeleteNode.java
@@ -0,0 +1,12 @@
+package linkedlist;
+
+/**
+ * ClassName:DeleteNode
+ * Package:linkedlist
+ * Description:
+ *
+ * @date:2020/7/21 11:22
+ * @author:TCY
+ */
+public class DeleteNode {
+}
---
+++ src/linkedlist/ListNode.java
@@ -0,0 +1,18 @@
+package linkedlist;
+
+/**
+ * ClassName:ListNode
+ * Package:linkedlist
+ * Description:
+ *
+ * @date:2020/7/21 20:11
+ * @author:TCY
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
